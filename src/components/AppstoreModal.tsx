9c372aac7e67bba79ad197328fa0cd15be5d04a5
Larry Ntori <l.ntori@generisdevelopers.com>
2023-02-17 15:01:54 +0000

    coming soon

--- src/components/AppstoreModal.tsx
+++ src/components/AppstoreModal.tsx
@@ -16,24 +16,25 @@ export default function AppstoreModal() {
       </button>
       {showModal ? (
         <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-sm">
               {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-[#2d2d2d] outline-none focus:outline-none">
+              <div className="relative flex flex-col w-full bg-[#2d2d2d] outline-none focus:outline-none">
                 {/*body*/}
-                <div className="relative p-6 flex-auto">
+                <div className="relative px-6 pb-6 flex-auto">
                   <Player
                     src="https://assets4.lottiefiles.com/packages/lf20_0skurerf.json"
                     className="player"
                     loop
                     autoplay
+                    style={{ height: '250px', width: '250px' }}
                   />
-                  <p className="my-4 text-white text-lg leading-relaxed">
+                  <p className="my-2 text-white text-lg leading-relaxed">
                     Coming Soon ....
                   </p>
                 </div>
                 {/*footer*/}
-                <div className="flex items-center justify-center p-6 rounded-b">
+                <div className="flex items-center justify-center px-6 pb-6 rounded-b">
                   <button
                     className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
--- src/components/PlaystoreModal.tsx
+++ src/components/PlaystoreModal.tsx
@@ -21,19 +21,20 @@ export default function PlaystoreModal() {
               {/*content*/}
               <div className="relative flex flex-col w-full bg-[#2d2d2d] outline-none focus:outline-none">
                 {/*body*/}
-                <div className="relative p-6 flex-auto">
+                <div className="relative px-6 pb-6 flex-auto">
                   <Player
                     src="https://assets4.lottiefiles.com/packages/lf20_0skurerf.json"
                     className="player"
                     loop
                     autoplay
+                    style={{ height: '250px', width: '250px' }}
                   />
-                  <p className="my-4 text-white text-lg leading-relaxed">
+                  <p className="my-2 text-white text-lg leading-relaxed">
                     Coming Soon ....
                   </p>
                 </div>
                 {/*footer*/}
-                <div className="flex items-center justify-center p-6 rounded-b">
+                <div className="flex items-center justify-center px-6 pb-6 rounded-b">
                   <button
                     className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
