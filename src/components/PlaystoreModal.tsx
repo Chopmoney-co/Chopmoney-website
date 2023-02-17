00cc1332caee985fb37c31f013131448f9951396
Larry Ntori <l.ntori@generisdevelopers.com>
2023-02-17 12:15:29 +0000

    coming soon

--- src/components/PlaystoreModal.tsx
+++ src/components/PlaystoreModal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaGooglePlay } from "react-icons/fa";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 export default function PlaystoreModal() {
   const [showModal, setShowModal] = React.useState(false);
@@ -18,20 +19,13 @@ export default function PlaystoreModal() {
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-sm">
               {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-[#2d2d2d] outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between p-5  rounded-t">
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
-                </div>
+              <div className="relative flex flex-col w-full bg-[#2d2d2d] outline-none focus:outline-none">
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
+                  <Player
+                    src="https://assets4.lottiefiles.com/packages/lf20_0skurerf.json"
+                    className="player"
+                  />
                   <p className="my-4 text-white text-lg leading-relaxed">
                     Coming Soon ....
                   </p>
@@ -43,7 +37,7 @@ export default function PlaystoreModal() {
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
-                    Close
+                    Alright
                   </button>
                 </div>
               </div>
