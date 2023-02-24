f79966642579417eacba1c18f4f5280a0b6222b6
Larry Ntori <l.ntori@generisdevelopers.com>
2023-02-24 16:22:55 +0000

    tc

--- src/pages/TermsC.tsx
+++ src/pages/TermsC.tsx
@@ -75,18 +75,54 @@ function TermsC() {
             </p>
           </div>
           <div>
-            <h1 className="my-12 text-xl font-bold leading-snug tracking-tight text-gray-800 md:text-5xl">
+            <h1 className="mt-12 mb-8 text-xl font-bold leading-snug tracking-tight text-gray-800 md:text-5xl">
               Terms of Use:
             </h1>
             <h2 className="mb-2 text-xl font-semibold leading-snug tracking-tight text-gray-800 md:text-2xl">
               By using our mobile app, you agree to the following terms:
             </h2>
-            <p className="mb-4 text-base font-normal text-gray-600">
-              Ever wondered if there is a way to just tie up all your code into
-              one single module for easy usage. If so, in this article I will
-              show you how to bundle all your code into a single javascript
-              module that you can easily use in any other project.
-            </p>
+            <ul className="list-disc">
+              <li className="mb-4 text-base font-normal text-gray-600">
+                You are responsible for maintaining the confidentiality of your
+                account and password.
+              </li>
+              <li className="mb-4 text-base font-normal text-gray-600">
+                You are responsible for all activities that occur under your
+                account
+              </li>
+              <li className="mb-4 text-base font-normal text-gray-600">
+                You agree to provide accurate and complete information when
+                creating an account You are responsible for maintaining the
+                confidentiality of your account and password.
+              </li>
+              <li className="mb-4 text-base font-normal text-gray-600">
+                You agree to use our app only for lawful purposes and in
+                accordance with these terms of use.
+              </li>
+              <li className="mb-4 text-base font-normal text-gray-600">
+                You agree to not use our app to engage in any illegal or
+                fraudulent activities.
+              </li>
+              <li className="mb-4 text-base font-normal text-gray-600">
+                You agree to indemnify and hold harmless Chopmoney and its
+                affiliates, officers, agents, and employees from any claim or
+                demand, including reasonable attorneys' fees, made by any third
+                party due to or arising out of your use of our app.
+              </li>
+              <li className="mb-4 text-base font-normal text-gray-600">
+                We reserve the right to terminate your account and access to our
+                app if you violate these terms of use.
+              </li>
+              <li className="mb-4 text-base font-normal text-gray-600">
+                We reserve the right to make changes to these terms of use at
+                any time.
+              </li>
+              <li className="mb-4 text-base font-normal text-gray-600">
+                By using Chopmoney, you acknowledge that you have read and
+                understood this Privacy Policy and our Terms of Use, and you
+                agree to be bound by them.
+              </li>
+            </ul>
           </div>
         </div>
       </section>
