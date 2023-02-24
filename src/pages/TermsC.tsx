d290cdc8ceff55708b216c71220628c4f5ceee23
Larry Ntori <l.ntori@generisdevelopers.com>
2023-02-24 16:00:49 +0000

    tc

--- src/pages/TermsC.tsx
+++ src/pages/TermsC.tsx
@@ -4,12 +4,12 @@ function TermsC() {
   return (
     <div>
       <section className="w-full px-4 py-24 mx-auto max-w-7xl md:w-3/4 lg:w-2/4">
-        <div className="flex flex-col space-y-12 divide-y divide-gray-200">
+        <div className="flex flex-col ">
           <div>
             <h1 className="mb-2 text-xl font-bold leading-snug tracking-tight text-gray-800 md:text-5xl">
               Privacy Policy
             </h1>
-            <p className="mb-4 text-base font-normal text-gray-600">
+            <p className="my-8 text-base font-normal text-gray-600">
               At Chopmoney, we are committed to protecting the privacy of our
               users. This Privacy Policy explains how we collect, use, disclose,
               and protect personal information through our mobile app and
@@ -17,88 +17,69 @@ function TermsC() {
             </p>
           </div>
           <div>
-            <p className="pt-12 mb-3 text-sm font-normal text-gray-500">
-              April 16, 2020
-            </p>
-            <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
-              <a href="#" className="text-gray-900 hover:text-purple-700">
-                Implement Dark Mode in Your Android App
-              </a>
+            <h2 className="mb-2 text-xl font-semibold leading-snug tracking-tight text-gray-800 md:text-2xl">
+              Collection of Personal Information:
             </h2>
             <p className="mb-4 text-base font-normal text-gray-600">
-              Are you curious to implement the Dark Mode in Android Application?
-              Here’s the perfect guideline to attain the Dark Mode in Android
-              Application. Don’t waste your time; just implement and enjoy Dark
-              Mode.
+              We collect personal information that you voluntarily provide to us
+              when you create an account, such as your name, email address,
+              phone number, and date of birth. We also collect financial
+              information, such as bank account details, transaction
+              information, and credit/debit card information, when you use our
+              app to make transactions. In addition, we may collect information
+              about your location, device type, and browsing history when you
+              use our app.
             </p>
-            <a href="#" className="btn btn-light btn-sm">
-              Continue Reading
-            </a>
           </div>
           <div>
-            <p className="pt-12 mb-3 text-sm font-normal text-gray-500">
-              April 16, 2020
-            </p>
-            <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
-              <a href="#" className="text-gray-900 hover:text-purple-700">
-                Why is Mental Health one of the Important Issues to Address?
-              </a>
+            <h2 className="mb-2 text-xl font-semibold leading-snug tracking-tight text-gray-800 md:text-2xl">
+              Use of Personal Information:
             </h2>
             <p className="mb-4 text-base font-normal text-gray-600">
-              Mental health was one of the under spoken topics before this
-              lockdown. After sitting at home for about six months I realized
-              that this is one of the important issues to address not only in
-              the work sector but also in daily living.
+              We use personal information to provide and improve our services,
+              as well as to communicate with you. We may also use your
+              information to send you promotional materials or to send you
+              updates on our app. We may also use your information for research
+              and analysis purposes, to improve our products and services and to
+              better understand our users’ needs.
             </p>
-            <a href="#" className="btn btn-light btn-sm">
-              Continue Reading
-            </a>
           </div>
           <div>
-            <p className="pt-12 mb-3 text-sm font-normal text-gray-500">
-              April 16, 2020
-            </p>
-            <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
-              <a href="#" className="text-gray-900 hover:text-purple-700">
-                Pattern Matching In Elixir
-              </a>
+            <h2 className="mb-2 text-xl font-semibold leading-snug tracking-tight text-gray-800 md:text-2xl">
+              Disclosure of Personal Information:
             </h2>
             <p className="mb-4 text-base font-normal text-gray-600">
-              Pattern matching is a great way to write idiomatic functional
-              code. It’s a powerful tenant of functional programming that makes
-              it a joy to write conditional statements. If you don’t want your
-              code to be peppered with deeply nested statements or multiple
-              variations of similar business logic, use pattern matching!
+              We will not share your personal information with third parties
+              without your consent, except as required by law or as described in
+              this Privacy Policy. We may share your personal information with
+              our affiliates and business partners, but only for the purpose of
+              providing our services to you. We may also share your personal
+              information with third-party service providers, such as payment
+              processors, but only to the extent necessary to perform their
+              services
             </p>
-            <a href="#" className="btn btn-light btn-sm">
-              Continue Reading
-            </a>
           </div>
           <div>
-            <p className="pt-12 mb-3 text-sm font-normal text-gray-500">
-              April 16, 2020
-            </p>
-            <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
-              <a href="#" className="text-gray-900 hover:text-purple-700">
-                3 things you should change during your focus group interview
-              </a>
+            <h2 className="mb-2 text-xl font-semibold leading-snug tracking-tight text-gray-800 md:text-2xl">
+              Security of Personal Information:
             </h2>
             <p className="mb-4 text-base font-normal text-gray-600">
-              We changed three things about our feedback sessions, and it
-              changed everything about running customer feedback sessions.
+              We take the security of your personal information seriously and
+              take appropriate measures to protect it from unauthorized access
+              or disclosure. We have implemented appropriate technical and
+              organizational measures to protect your personal information from
+              loss, theft, unauthorized access, and unauthorized alteration.
+              However, please note that no method of transmitting or storing
+              data is completely secure, and we cannot guarantee the absolute
+              security of your personal information
             </p>
-            <a href="#" className="btn btn-light btn-sm">
-              Continue Reading
-            </a>
           </div>
           <div>
-            <p className="pt-12 mb-3 text-sm font-normal text-gray-500">
-              April 16, 2020
-            </p>
-            <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
-              <a href="#" className="text-gray-900 hover:text-purple-700">
-                Using Webpack with React Typescript
-              </a>
+            <h1 className="my-12 text-xl font-bold leading-snug tracking-tight text-gray-800 md:text-5xl">
+              Terms of Use:
+            </h1>
+            <h2 className="mb-2 text-xl font-semibold leading-snug tracking-tight text-gray-800 md:text-2xl">
+              By using our mobile app, you agree to the following terms:
             </h2>
             <p className="mb-4 text-base font-normal text-gray-600">
               Ever wondered if there is a way to just tie up all your code into
@@ -106,9 +87,6 @@ function TermsC() {
               show you how to bundle all your code into a single javascript
               module that you can easily use in any other project.
             </p>
-            <a href="#" className="btn btn-light btn-sm">
-              Continue Reading
-            </a>
           </div>
         </div>
       </section>
