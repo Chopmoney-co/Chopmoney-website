6f562f352b6e02dc16cd0ba7a003ad8c75982d09
oopokubadu <opokubadu18@gmail.com>
2023-06-20 08:24:45 +0000

    okata

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -22,12 +22,9 @@ function TermsC() {
             </h2>
             <p className="mb-4 text-base font-normal text-gray-600">
               We collect personal information that you voluntarily provide to us
-              when you create an account, such as your name, email address,
-              phone number, and date of birth. We also collect financial
-              information, such as bank account details, transaction
-              information, and credit/debit card information, when you use our
-              app to make transactions. In addition, we may collect information
-              about your location, device type, and browsing history when you
+              when you create an account, such as your name, email address, and
+              phone number. In addition, we may collect information
+              about your location, and device type when you
               use our app.
             </p>
           </div>
@@ -56,7 +53,7 @@ function TermsC() {
               providing our services to you. We may also share your personal
               information with third-party service providers, such as payment
               processors, but only to the extent necessary to perform their
-              services
+              services.
             </p>
           </div>
           <div>
