3ad49378904fb29a4aa0d3c4c772a680482d6ab8
Larry Ntori <l.ntori@generisdevelopers.com>
2023-07-11 21:05:36 +0000

    chopmoney

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Logo from "../assets/Group 2166.png";
 
 function Support() {
   return (
     <div>
       <section className="w-full px-4 py-24 mx-auto max-w-7xl md:w-3/4 lg:w-2/4">
+        <a href="/">
+          <img className="mb-16" src={Logo} alt="" />
+        </a>
         <div className="flex flex-col ">
           <div>
             <h1 className="mb-2 text-xl font-bold leading-snug tracking-tight text-gray-800 md:text-5xl">
@@ -15,10 +19,16 @@ function Support() {
             </p>
           </div>
           <div className="flex flex-col">
-            <a href="tel:+233555190488" className="mb-2 text-base font-normal text-gray-600">
+            <a
+              href="tel:+233555190488"
+              className="mb-2 text-base font-normal text-gray-600"
+            >
               +233555190488
             </a>
-            <a href="tel:+233200438498" className="mb-2 text-base font-normal text-gray-600">
+            <a
+              href="tel:+233200438498"
+              className="mb-2 text-base font-normal text-gray-600"
+            >
               +233200438498
             </a>
             <a
@@ -29,7 +39,10 @@ function Support() {
             </a>
             <p className="mt-2 text-base font-normal text-gray-600">
               WhatsApp chat:{" "}
-              <a className="underline" href="https://wa.me/message/NRDZZQVYGWMCL1">
+              <a
+                className="underline"
+                href="https://wa.me/message/NRDZZQVYGWMCL1"
+              >
                 https://wa.me/message/NRDZZQVYGWMCL1
               </a>
             </p>
