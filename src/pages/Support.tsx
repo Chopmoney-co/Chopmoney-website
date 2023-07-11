008466671f933a4f0f6250615c9371656b99116e
Larry Ntori <l.ntori@generisdevelopers.com>
2023-07-11 22:07:29 +0000

    chopmoney

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -3,17 +3,17 @@ import Logo from "../assets/Group 2166.png";
 
 function Support() {
   return (
-    <div>
+    <div className="bg-[#ECEC08] h-[100vh]">
       <section className="w-full px-4 py-24 mx-auto max-w-7xl md:w-3/4 lg:w-2/4">
         <a className="" href="/">
           <img className="mb-16 mx-auto" src={Logo} alt="" />
         </a>
         <div className="flex flex-col ">
           <div>
-            <h1 className="mb-2 text-xl font-bold text-center leading-snug text-center tracking-tight text-gray-800 md:text-5xl">
+            <h1 className="mb-2 text-xl font-bold text-center leading-snug text-center tracking-tight text-black md:text-5xl">
               Support
             </h1>
-            <p className="my-8 text-base font-normal text-center text-gray-600">
+            <p className="my-8 text-base font-normal text-center text-black">
               Support superheroes at your service! Questions? Issues? We've got
               your back. Satisfaction guaranteed!
             </p>
@@ -21,25 +21,25 @@ function Support() {
           <div className="flex flex-col">
             <a
               href="tel:+233555190488"
-              className="mb-2 text-base font-normal text-center text-gray-600"
+              className="mb-2 text-base font-normal text-center text-black"
             >
               +233555190488
             </a>
             <a
               href="tel:+233200438498"
-              className="mb-2 text-base font-normal text-center text-gray-600"
+              className="mb-2 text-base font-normal text-center text-black"
             >
               +233200438498
             </a>
             <a
               href="mailto:admin@chopmoney.co"
-              className="mb-4 text-base font-normal text-center text-gray-600"
+              className="mb-4 text-base font-normal text-center text-black"
             >
               admin@chopmoney.co
             </a>
           
               <a
-                className=" mt-2 bg-[#ECEC08] w-48 rounded-xl mx-auto p-4 text-base font-normal text-center text-gray-700"
+                className=" mt-2 bg-black w-48 rounded-xl mx-auto p-4 text-base font-normal text-center text-[#ECEC08]"
                 href="https://wa.me/message/NRDZZQVYGWMCL1"
               >
               Whatsapp Chat
